bf20388d34c813e3a49058908d945940602d8859
Joe Lindsay <joe.lindsay20@gmail.com>
2017-12-16 21:54:10 -0800

    Added Stripe page
    learned how to test locally
    learned how to link between pages
    learned how to take user inputs
    Added /stripe

--- app.rb
+++ app.rb
@@ -1,25 +1,75 @@
 require "rubygems"
 require "sinatra"
-#require "stripe"
+require "stripe"
 
+set :port, 8080
+set :static, true
+set :public_folder, "static"
+set :views, "views"
 
-#Stripe.api_key = "sk_test_ECAvVBtdKXp9xUaX8V7XjOmf"
+set :publishable_key, 'pk_test_tIN888KxbhuQn3Yq9Ehsy9uu'
+set :secret_key, 'sk_test_ECAvVBtdKXp9xUaX8V7XjOmf'
+
+Stripe.api_key = settings.secret_key
 
 get '/' do
-  "Hello world!"
+  erb :index
 end
 
-get '/fun' do
-  "Show me what you got!"
+get '/stripe' do
+  # "Show me what you had today!"
+   erb :checkout
+end
+
+
+post '/charge' do
+  # Amount in cents
+  @amount = 500
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :source  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge',
+    :currency    => 'usd',
+    :customer    => customer.id
+  )
+
+  erb :charge
+end
+
+get '/hello' do
+    erb :hello_form
+end
+
+post '/hello' do
+    greeting = params[:greeting] || "Hi There"
+    name = params[:name] || "Nobody"
+
+    erb :greet, :locals => {'greeting' => greeting, 'name' => name}
 end
 
 get '/number' do
-  p "Hello world! Give me a number."
+  "Hello world! Give me a number."
 
 num = gets.to_i
 
-puts "Your number times 2 is " + (num * 2).to_s
+  "Your number times 2 is " + (num * 2).to_s
 end
+
+__END__
+
+@@ layout
+  <!DOCTYPE html>
+  <html>
+  <head></head>
+  <body>
+    <%= yield %>
+  </body>
+  </html>
 
 # puts "Enter the pennies you want to pay."
 #
